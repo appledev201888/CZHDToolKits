7dfc16f8f5d452ff551c8792751d37c988b3dda3
chenweibin <weibinchen@uutaka.com>
2019-01-12 21:14:02 +0800

    提交的描述说明

---
+++ Classes/CZHToolKits.framework/Headers/CZHToolKits.h
@@ -0,0 +1,14 @@
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CZHToolKits.
+FOUNDATION_EXPORT double CZHToolKitsVersionNumber;
+
+//! Project version string for CZHToolKits.
+FOUNDATION_EXPORT const unsigned char CZHToolKitsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CZHToolKits/PublicHeader.h>
+
+#import <CZHToolKits/CZHToolKitsManage.h>
+
+
---
+++ Classes/CZHToolKits.framework/Headers/CZHToolKitsManage.h
@@ -0,0 +1,13 @@
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CZHToolKitsManage : NSObject
+
++ (void)CZHToolKitsManage:(void(^)(NSString *temp))block;
+
+@end
+
+NS_ASSUME_NONNULL_END
